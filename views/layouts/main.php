a84dd3644323101b2feb378fdc5c7ec75f742724
Serge <popov.ser92@gmail.com>
2017-12-09 00:52:19 +0200

    update CashFlowStatementGroup

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -145,10 +145,10 @@ $bundle = yiister\gentelella\assets\Asset::register($this);
 											"label" => "Статьи ДДС",
 											"url" => "/cash-flow-statement/index",
 										],
-										[
-											"label" => "Группы статей ДДС",
-											"url" => "/cash-flow-statement-group/index",
-										],
+//										[
+//											"label" => "Группы статей ДДС",
+//											"url" => "/cash-flow-statement-group/index",
+//										],
 									],
 								],
 							],
