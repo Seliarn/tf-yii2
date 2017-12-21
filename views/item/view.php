a0b80cb5f8c72dce6085b92fdb2e11d167aa73ff
Serge <popov.ser92@gmail.com>
2017-12-21 14:34:58 +0200

    item fix

--- views/item/view.php
+++ views/item/view.php
@@ -6,38 +6,55 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Item */
 
-$this->title = $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Items', 'url' => ['index']];
+$this->title = $model::$titles['rus']['main'] . ' № ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => $model::$titles['rus']['plural'], 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="item-view">
+<div class = "item-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+	<p>
+		<?php
+		echo Html::a(Yii::$app->params['translate']['rus']['btn-back-to-list'], ['index'], ['class' => 'btn btn-primary']);
+		echo Html::a(Yii::$app->params['translate']['rus']['btn-update'], ['update', 'id' => $model->id], ['class' => 'btn btn-primary']);
+		echo Html::a(Yii::$app->params['translate']['rus']['btn-create'], ['create'], ['class' => 'btn btn-primary']);
+		echo Html::a(Yii::$app->params['translate']['rus']['btn-delete'], ['delete', 'id' => $model->id], [
+			'class' => 'btn btn-danger',
+			'data' => [
+				'confirm' => Yii::$app->params['translate']['rus']['dialog-are-you-sure'],
+				'method' => 'post',
+			],
+		]);
+		?>
+	</p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            'group_id',
-            'measures',
-            'state',
-            'created',
-            'updated',
-            'status',
-            'note',
-        ],
-    ]) ?>
+	<?=
+	DetailView::widget([
+		'model' => $model,
+		'attributes' => [
+			'id',
+			'title',
+			[
+				'label' => $model->attributeLabels('group_id'),
+				'value' => (!$model->getGroup()->one()) ? '' : $model->getGroup()->one()->title
+			],
+			[
+				'label' => $model->attributeLabels('measures'),
+				'value' => $model->getMeasuresAlias()
+			],
+			[
+				'label' => $model->attributeLabels('state'),
+				'value' => $model->getStateAlias()
+			],
+			'note',
+			[
+				'label' => $model->attributeLabels('status'),
+				'value' => $model->getStatusAlias()
+			],
+			'created:datetime',
+			'updated:datetime',
+		],
+	]) ?>
 
 </div>
