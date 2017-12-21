4e728068f715dad0a5fa1717f996c871ee42dcf2
Serge <popov.ser92@gmail.com>
2017-12-21 13:02:26 +0200

    update models

--- models/SubcontoModel.php
+++ models/SubcontoModel.php
@@ -19,59 +19,60 @@ use Yii;
  */
 class SubcontoModel extends LoggedActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return 'Subconto_Model';
-    }
+	static $titles = [
+		'rus' => [
+			'main' => 'План счетов',
+			'plural' => 'Планы счетов'
+		]
+	];
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['class_name'], 'required'],
-            [['status'], 'integer'],
-            [['created', 'updated'], 'safe'],
-            [['class_name', 'title'], 'string', 'max' => 255],
-            [['note'], 'string', 'max' => 100],
-            [['class_name'], 'unique'],
-        ];
-    }
+	static $labels = [
+		'id' => 'ID',
+		'class_name' => 'Class Name',
+		'title' => 'Название',
+		'status' => 'Статус',
+		'created' => 'Создан',
+		'updated' => 'Изменен',
+		'note' => 'Примечание',
+	];
 
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => 'ID',
-            'class_name' => 'Class Name',
-            'title' => 'Title',
-            'status' => 'Status',
-            'created' => 'Created',
-            'updated' => 'Updated',
-            'note' => 'Note',
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return 'Subconto_Model';
+	}
 
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getAccountBooks()
-    {
-        return $this->hasMany(AccountBook::className(), ['subconto_model_id' => 'id']);
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['class_name'], 'required'],
+			[['status'], 'integer'],
+			[['created', 'updated'], 'safe'],
+			[['class_name', 'title'], 'string', 'max' => 255],
+			[['note'], 'string', 'max' => 100],
+			[['class_name'], 'unique'],
+		];
+	}
 
-    /**
-     * @inheritdoc
-     * @return \app\models\aq\SubcontoModelQuery the active query used by this AR class.
-     */
-    public static function find()
-    {
-        return new \app\models\aq\SubcontoModelQuery(get_called_class());
-    }
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getAccountBooks()
+	{
+		return $this->hasMany(AccountBook::className(), ['subconto_model_id' => 'id']);
+	}
+
+	/**
+	 * @inheritdoc
+	 * @return \app\models\aq\SubcontoModelQuery the active query used by this AR class.
+	 */
+	public static function find()
+	{
+		return new \app\models\aq\SubcontoModelQuery(get_called_class());
+	}
 }
