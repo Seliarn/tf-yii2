70ba90e91454c1025731255062d60ae7142b942a
Serge <popov.ser92@gmail.com>
2018-02-04 20:28:49 +0200

    migrate alter items column

---
+++ migrations/m180204_182807_alter_items_columns.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180204_182807_alter_items_columns
+ */
+class m180204_182807_alter_items_columns extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180204_182807_alter_items_columns cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180204_182807_alter_items_columns cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
